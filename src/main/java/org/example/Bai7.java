3f7196b147b3c138e2e3544e8c8ad9809403f962
lananh <anhbl140497@gmail.com>
2022-11-20 14:39:42 +0700

    Bai tap part 1

---
+++ src/main/java/org/example/Bai7.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Bai7 {
+    public void demKiTu() {
+        String chuoi;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập vào một chuỗi : ");
+        chuoi = sc.nextLine();
+        int dem =0;
+        for (int i =0; i< chuoi.length();i++){
+            if('a' == chuoi.charAt(i)){
+                dem++;
+            }
+        }
+        System.out.print("Kí tự a xuất hiện trong chuỗi: "+ dem + " lần");
+    }
+}
